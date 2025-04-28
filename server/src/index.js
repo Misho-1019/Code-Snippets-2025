65d9160f05328339ff945ddc8c1584f27def21ac
Misho1019 <mikailtorres99@gmail.com>
2025-04-28 20:16:43 +0300

    Install and set up dotenv

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,14 @@
 import express from "express";
+import dotenv from 'dotenv';
 
 const app = express();
+
+dotenv.config();
 
 app.get('/', (req, res) => {
     res.send('It works!')
 })
 
-const PORT = 3030;
+const port = process.env.PORT || 3030;
 
-app.listen(PORT, () => console.log(`Server is listening on http://localhost:${PORT}...`))
+app.listen(port, () => console.log(`Server is listening on http://localhost:${port}...`))
