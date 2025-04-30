355e926c0b129dd9204ee9e124a8f835795e2090
Misho1019 <mikailtorres99@gmail.com>
2025-04-30 06:30:59 +0300

    Add snippet controller in routes

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import authController from "./controllers/authController.js";
+import snippetController from "./controllers/snippetController.js";
 
 const routes = Router();
 
 routes.use('/auth', authController)
+routes.use('/snippets', snippetController)
 
 export default routes;
