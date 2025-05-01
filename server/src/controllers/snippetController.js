790b4e4f1ed60592285afd66303ba2a31dfab322
Misho1019 <mikailtorres99@gmail.com>
2025-05-01 04:55:13 +0300

    Fix getAll functionality

--- server/src/controllers/snippetController.js
+++ server/src/controllers/snippetController.js
@@ -9,12 +9,18 @@ snippetController.get('/', async (req, res) => {
     const page = Number(req.query.page) || 1;
     const limit = Number(req.query.limit) || 10;
 
-    const { title, description, langauge } = req.query
+    const { title, description, language } = req.query
+
+    const filter = {};
+
+    if (title) filter.title;
+    if (description) filter.description;
+    if (language) filter.language;
 
     try {
         const [snippets, totalCount] = await Promise.all([
-            snippetService.getAll(page, limit, { title, description, langauge }),
-            snippetService.getTotalCount({ title, description, langauge })
+            snippetService.getAll(page, limit, filter),
+            snippetService.getTotalCount(filter)
         ])
 
         res.status(200).json({
