ad6b0eb676b23110f7a3f55a9f86363bbda53be2
Misho1019 <mikailtorres99@gmail.com>
2025-05-04 17:08:07 +0300

    Add user register

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -27,11 +27,11 @@ export default function Login() {
                 <form className="space-y-4" action={loginAction}>
                     <div>
                         <label className="block text-sm font-medium text-gray-700">Email</label>
-                        <input type="email" name="email" className="mt-1 w-full px-4 py-2 border rounded-md focus:ring-2 focus:ring-indigo-500" />
+                        <input type="email" name="email" className="mt-1 w-full px-4 py-2 border rounded-md focus:ring-2 focus:ring-indigo-500" placeholder="Email..."/>
                     </div>
                     <div>
                         <label className="block text-sm font-medium text-gray-700">Password</label>
-                        <input type="password" name="password" className="mt-1 w-full px-4 py-2 border rounded-md focus:ring-2 focus:ring-indigo-500" />
+                        <input type="password" name="password" className="mt-1 w-full px-4 py-2 border rounded-md focus:ring-2 focus:ring-indigo-500" placeholder="Password..."/>
                     </div>
                     <button type="submit" className="w-full bg-indigo-600 text-white py-2 rounded-md hover:bg-indigo-700 transition" disabled={isPending}>
                         Login
--- client/src/components/register/Register.jsx
+++ client/src/components/register/Register.jsx
@@ -1,22 +1,51 @@
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
+import { useRegister } from "../../api/authApi";
+import { useContext } from "react";
+import { UserContext } from "../../context/UserContext";
 
 export default function Register() {
+    const navigate = useNavigate();
+    const { register } = useRegister()
+    const { userLoginHandler } = useContext(UserContext)
+
+    const registerHandler = async (formData) => {
+        const values = Object.fromEntries(formData)
+
+        const rePassword = formData.get('re-password')
+
+        if (rePassword !== values.password) {
+            console.log('Password mismatch!');
+            
+            return
+        }
+
+        const authData = await register(values.username, values.email, values.password)
+
+        userLoginHandler(authData)
+        
+        navigate('/')
+    }
+
     return (
         <main className="min-h-screen flex items-center justify-center bg-gray-50 px-4 py-12">
             <div className="w-full max-w-md bg-white p-8 shadow-md rounded-2xl">
                 <h2 className="text-2xl font-bold text-gray-800 text-center mb-6">Create an Account</h2>
-                <form className="space-y-4">
+                <form className="space-y-4" action={registerHandler}>
                     <div>
                         <label className="block text-sm font-medium text-gray-700">Username</label>
-                        <input type="text" className="mt-1 w-full px-4 py-2 border rounded-md focus:ring-2 focus:ring-indigo-500" />
+                        <input type="text" name="username" className="mt-1 w-full px-4 py-2 border rounded-md focus:ring-2 focus:ring-indigo-500" placeholder="Username..." />
                     </div>
                     <div>
                         <label className="block text-sm font-medium text-gray-700">Email</label>
-                        <input type="email" className="mt-1 w-full px-4 py-2 border rounded-md focus:ring-2 focus:ring-indigo-500" />
+                        <input type="email" name="email" className="mt-1 w-full px-4 py-2 border rounded-md focus:ring-2 focus:ring-indigo-500" placeholder="Email..."/>
                     </div>
                     <div>
                         <label className="block text-sm font-medium text-gray-700">Password</label>
-                        <input type="password" className="mt-1 w-full px-4 py-2 border rounded-md focus:ring-2 focus:ring-indigo-500" />
+                        <input type="password" name="password" className="mt-1 w-full px-4 py-2 border rounded-md focus:ring-2 focus:ring-indigo-500" placeholder="Password..."/>
+                    </div>
+                    <div>
+                        <label className="block text-sm font-medium text-gray-700">Repeat Password</label>
+                        <input type="password" name="re-password" className="mt-1 w-full px-4 py-2 border rounded-md focus:ring-2 focus:ring-indigo-500" placeholder="Repeat password..."/>
                     </div>
                     <button type="submit" className="w-full bg-indigo-600 text-white py-2 rounded-md hover:bg-indigo-700 transition">
                         Register
