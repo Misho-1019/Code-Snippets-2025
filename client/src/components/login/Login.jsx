127177374a72373162cad30e0f5b7d4e18fb6969
Misho1019 <mikailtorres99@gmail.com>
2025-05-06 06:57:02 +0300

    Add error validation on Login form

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,39 +1,90 @@
 import { useActionState, useContext } from "react";
 import { Link, useNavigate } from "react-router";
 import { useLogin } from "../../api/authApi";
-import { UserContext, useUserContext } from "../../context/UserContext";
+import { useUserContext } from "../../context/UserContext";
+import { toast } from "react-toastify";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+const schema = yup.object({
+    email: yup.string().email('Invalid email format!'),
+    password: yup.string().min(6, 'Password must be at least 6 characters!'),
+})
 
 export default function Login() {
     const navigate = useNavigate();
     const { login } = useLogin();
     const { userLoginHandler } = useUserContext()
 
-    const loginHandler = async (_, formData) => {
-        const values = Object.fromEntries(formData)
+    const {
+        register,
+        handleSubmit,
+        formState: { isSubmitting },
+    } = useForm({
+        resolver: yupResolver(schema),
+    })
 
-        const authData = await login(values.email, values.password)
+    const loginHandler = async (data) => {
+        try {
+            const authData = await login(data.email, data.password)
 
-        userLoginHandler(authData)
+            userLoginHandler(authData)
 
-        navigate('/')
+            toast.success('Successful login!', {
+                position: 'top-center',
+                autoClose: 2000,
+                theme: 'dark',
+            })
+
+            navigate('/')
+        } catch (error) {
+            toast.error(error.message || 'Login failed!', {
+                position: 'top-center',
+                autoClose: 2000,
+                theme: 'dark',
+            })
+        }
     }
 
-    const [_, loginAction, isPending] = useActionState(loginHandler, { email: '', password: '' })
+    // const loginHandler = async (_, formData) => {
+    //     const values = Object.fromEntries(formData)
+
+    //     const authData = await login(values.email, values.password)
+
+    //     userLoginHandler(authData)
+
+    //     navigate('/')
+    // }
+
+    const onError = (errors) => {
+        const firstError = Object.values(errors)[0]
+
+        if (firstError?.message) {
+            toast.error(firstError.message, {
+                position: 'top-center',
+                autoClose: 2000,
+                theme: 'dark',
+            })
+        }
+    }
+
+    // const [_, loginAction, isPending] = useActionState(loginHandler, { email: '', password: '' })
 
     return (
         <main className="min-h-screen flex items-center justify-center bg-gray-50 px-4 py-12">
             <div className="w-full max-w-md bg-white p-8 shadow-md rounded-2xl">
                 <h2 className="text-2xl font-bold text-gray-800 text-center mb-6">Login to Your Account</h2>
-                <form className="space-y-4" action={loginAction}>
+                <form className="space-y-4" onSubmit={handleSubmit(loginHandler, onError)} noValidate>
                     <div>
                         <label className="block text-sm font-medium text-gray-700">Email</label>
-                        <input type="email" name="email" className="mt-1 w-full px-4 py-2 border rounded-md focus:ring-2 focus:ring-indigo-500" placeholder="Email..."/>
+                        <input type="email" name="email" className="mt-1 w-full px-4 py-2 border rounded-md focus:ring-2 focus:ring-indigo-500" {...register('email')} placeholder="Email..."/>
                     </div>
                     <div>
                         <label className="block text-sm font-medium text-gray-700">Password</label>
-                        <input type="password" name="password" className="mt-1 w-full px-4 py-2 border rounded-md focus:ring-2 focus:ring-indigo-500" placeholder="Password..."/>
+                        <input type="password" name="password" className="mt-1 w-full px-4 py-2 border rounded-md focus:ring-2 focus:ring-indigo-500" {...register('password')} placeholder="Password..."/>
                     </div>
-                    <button type="submit" className="w-full bg-indigo-600 text-white py-2 rounded-md hover:bg-indigo-700 transition" disabled={isPending}>
+                    <button type="submit" className="w-full bg-indigo-600 text-white py-2 rounded-md hover:bg-indigo-700 transition" disabled={isSubmitting}>
                         Login
                     </button>
                 </form>
